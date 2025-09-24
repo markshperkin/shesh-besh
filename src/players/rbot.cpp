49acb61246ce411feb8feadf88c703a6abfd4c1d
mark shperkin <markshperkin1@gmail.com>
2025-09-24 18:14:52 -0400

    bug fix

--- src/players/rbot.cpp
+++ src/players/rbot.cpp
@@ -4,7 +4,6 @@
 namespace bg {
 
     Step RBot::makeStep(const std::vector<Step>& steps) {
-        if (steps.empty()) throw std::runtime_error("RBot::makeStep called with empty steps");
         std::uniform_int_distribution<int> dist(0, steps.size() - 1);
         return steps[dist(rng_)];
     }
